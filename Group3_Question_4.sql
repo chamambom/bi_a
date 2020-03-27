5f09a8f7a37aa3f250dd026a25fce7bf83b98748
Martin Chamambo <chamambom@gmail.com>
2020-03-27 20:12:36 +0200

    Added screenshot for star schema

---
+++ Group3_Question_4.sql
@@ -0,0 +1,11 @@
+SELECT count( * ) as  categories_total_record FROM categories
+
+SELECT count( * ) as  customers_total_record FROM customers
+
+SELECT count( * ) as  departments_total_record FROM departments
+
+SELECT count( * ) as  order_items_total_record FROM order_items
+
+SELECT count( * ) as  orders_total_record FROM orders
+
+SELECT count( * ) as  products_total_record FROM products
